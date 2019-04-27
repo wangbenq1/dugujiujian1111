abb9dbff5e603623cd109f3bc70f422951d2e47d
wangbenq <wbq_916934@163.com>
2019-04-27 21:01:28 +0800

    333333333

--- src/main/java/com/atguigu/jianfa1111/DugujiujianApp.java
+++ src/main/java/com/atguigu/jianfa1111/DugujiujianApp.java
@@ -5,8 +5,6 @@ public class DugujiujianApp {
         System.out.println("111111111111111111111111");
         System.out.println("111111111111111111111111");
         System.out.println("111111111111111111111111");
-        System.out.println("222222222222222222222222");
-        System.out.println("222222222222222222222222");
         System.out.println("333333333333333333333333");
         System.out.println("333333333333333333333333");
         System.out.println("333333333333333333333333");
